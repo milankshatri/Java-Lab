18034e0b61e05b561b8649194e19e3e7f582b054
Milan Chhetri <46187103+milankshatri@users.noreply.github.com>
2020-02-10 21:01:03 +0545

    Create DefaultConstructor.java

---
+++ Lab_2/DefaultConstructor.java
@@ -0,0 +1,12 @@
+class Multiplication{
+    Multiplication(){
+        int Var1=6,Var2=7;
+        System.out.println("The product of Var1 and Var2 = "+(Var1*Var2));
+    }
+}
+class DefaultConstructor{
+    public static void main(String[] args)
+    {
+        Multiplication obj=new Multiplication();
+    }
+}
