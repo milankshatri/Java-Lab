fbd5a3cdd2ef63b83563e1838a8917c896fa08f2
Milan Chhetri <46187103+milankshatri@users.noreply.github.com>
2020-02-10 19:40:41 +0545

    Create TwoDimensionalArray.java

---
+++ Lab_1/TwoDimensionalArray.java
@@ -0,0 +1,12 @@
+class TwoDimensionalArray{
+	public static void main(String[] args) {
+		int a[][]={{4,5},{4,9}};
+		int i,j;
+		System.out.println("The numbers in array are:");
+		for(i=0;i<=1;i++){
+			for(j=0;j<=1;j++){
+				System.out.println(a[i][j]);
+			}
+		}
+	}
+}
