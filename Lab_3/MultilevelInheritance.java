406de1d569ae2503432f9051244c4b97a16239e4
Milan Chhetri <46187103+milankshatri@users.noreply.github.com>
2020-02-10 22:02:46 +0545

    Update MultilevelInheritance.java

--- Lab_3/MultilevelInheritance.java
+++ Lab_3/MultilevelInheritance.java
@@ -1,1 +1,26 @@
-
+class Hello{
+	void G1()
+	{
+		System.out.println("Hello");
+	}
+}
+class There extends Hello{
+	void G2()
+	{
+		System.out.println("There");
+	}
+}
+class StarWars extends There{
+	void G3()
+	{
+		System.out.println("General");
+	}
+}
+class MultilevelInheritance{
+	public static void main(String[] args) {
+		StarWars o=new StarWars();
+		o.G1();
+		o.G2();
+		o.G3();
+	}
+}
