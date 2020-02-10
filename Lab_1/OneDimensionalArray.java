bb010e9d241f82d14e0d1c9b75f805abeecf54b9
Milan Chhetri <46187103+milankshatri@users.noreply.github.com>
2020-02-10 19:27:08 +0545

    Create OneDimensionalArray.java

---
+++ Lab_1/OneDimensionalArray.java
@@ -0,0 +1,9 @@
+class OneDimensionalArray{
+	public static void main(String[] args) {
+		int a[]={3,4,5,6},i;
+		System.out.println("The numbers in array are:");
+		for(i=0;i<=4;i++){
+			System.out.println(a[i]);
+		}
+	}
+}
