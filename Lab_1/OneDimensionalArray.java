9e99337912bf080cd6943db353e24ef00479ed99
Milan Chhetri <46187103+milankshatri@users.noreply.github.com>
2020-02-10 19:43:05 +0545

    Update OneDimensionalArray.java

--- Lab_1/OneDimensionalArray.java
+++ Lab_1/OneDimensionalArray.java
@@ -2,7 +2,7 @@ class OneDimensionalArray{
 	public static void main(String[] args) {
 		int a[]={3,4,5,6},i;
 		System.out.println("The numbers in array are:");
-		for(i=0;i<=4;i++){
+		for(i=0;i<=3;i++){
 			System.out.println(a[i]);
 		}
 	}
