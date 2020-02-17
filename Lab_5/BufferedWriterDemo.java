4c686480d0f6b50265f7ad1cbef838828ae451bb
Milan Chhetri <46187103+milankshatri@users.noreply.github.com>
2020-02-17 21:10:58 +0545

    Create BufferedWriterDemo.java

---
+++ Lab_5/BufferedWriterDemo.java
@@ -0,0 +1,17 @@
+import java.io.*;
+class BufferedWriterDemo{
+	public static void main(String[] args) {
+		try{
+			FileWriter fwd=new FileWriter("Buffer.txt");
+			BufferedWriter bfr=new BufferedWriter(fwd);
+			bfr.write("Hello There");
+			bfr.newLine();
+			bfr.write("General");
+			bfr.flush();
+			bfr.close();
+		}catch(Exception e){
+			System.out.println(e);
+		}
+		System.out.println("Completed");
+	}
+}
